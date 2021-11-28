27f53eb89afbd7181e439104764e07ad4a867f3f
arvin361 <arvin_361@hotmail.com>
2021-11-28 17:36:00 -0500

    Create boilerplate and folder structure

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.scss";
+
+export default function Home() {
+  return (
+    <>
+      <div className="page">
+        <section className="page__section">
+          <h1>I AM HOME PAGE</h1>
+        </section>
+      </div>
+    </>
+  );
+}
