8850414c39198d786c31f06122a38fc7bc9257ab
arvin361 <arvin_361@hotmail.com>
2021-11-28 23:42:02 -0500

    Add enter functionality on submit and map through array of synonyms for each word submitted

--- src/App.js
+++ src/App.js
@@ -11,16 +11,23 @@ function App() {
 
   // Function to fetch information on button
   // click, and set the data accordingly
-  function getSynonym() {
+  const getSynonym = (e) => {
     Axios.get(apiURL).then((response) => {
       setData(response.data[0]);
     });
-  }
+  };
 
-  // const submitHandler = (e) => {
-  //   // Prevent form submission on Enter key
-  //   e.preventDefault();
-  // };
+  const handleChange = (e) => {
+    setSearchWord(e.target.value);
+  };
+
+  const handleKeypress = (e) => {
+    //it triggers by pressing the enter key
+    var key = e.keyCode || e.which;
+    if (key === 13) {
+      getSynonym();
+    }
+  };
 
   // const Synonym = ({ searchWord }) => <li>{searchWord}</li>;
 
@@ -32,20 +39,14 @@ function App() {
           <input
             type="text"
             placeholder="&nbsp;"
-            onChange={(event) => {
-              setSearchWord(event.target.value);
-            }}
+            onChange={handleChange}
+            onKeyPress={handleKeypress}
           />
           <span className="label">Whats Another Word For...</span>
           <span className="focus-bg"></span>
         </label>
 
-        <button
-          className="page__button"
-          onClick={() => {
-            getSynonym();
-          }}
-        >
+        <button className="page__button" onClick={getSynonym} type="submit">
           GO
         </button>
       </div>
@@ -54,6 +55,13 @@ function App() {
         <div className="page__displayResults">
           <h2 className="page__word">{data.word} </h2>
           <h4 className="page__subtitle">Synonyms:</h4>
+          <ul>
+            {data.meanings[1].definitions[0].synonyms.map((synonym, i) => (
+              <li className="page__synonym" key={i}>
+                {synonym}
+              </li>
+            ))}
+          </ul>
           <h4 className="page__subtitle">Definition:</h4>
           <p>{data.meanings[0].definitions[0].definition}</p>
           <h4 className="page__subtitle">Parts of speech:</h4>
@@ -63,12 +71,6 @@ function App() {
           {console.log(data.meanings[1].definitions[0].synonyms)}
           {/* <p>{data.meanings[1].definitions[0].synonyms}</p> */}
           {/* {data.meanings[1].definitions[0].synonyms}.map{} */}
-
-          {/* <ul>
-            {data.map((synonyms, i) => (
-              <li key={i}>{synonym}</li>
-            ))}
-          </ul> */}
 
           {/* <ul>
             {data.meanings[1].definitions[0].synonyms.map((searchWord, i) => (
