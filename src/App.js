45cf02808c1318b4c102db57b2a317d35e113cf9
arvin361 <arvin_361@hotmail.com>
2021-11-28 22:57:43 -0500

    Import axios to use dictionary api and use react hooks to get synonyms, definition and other information about a word, and style app.js page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,84 @@
-// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { React, useState } from "react";
+import Axios from "axios";
 import "./App.scss";
-import Home from "./pages/Home/Home";
 
 function App() {
-  return <Home />;
+  // Use react hook 'useState' to set up initial state
+  const [data, setData] = useState("");
+  const [searchWord, setSearchWord] = useState("");
+
+  const apiURL = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${searchWord}`;
+
+  // Function to fetch information on button
+  // click, and set the data accordingly
+  function getSynonym() {
+    Axios.get(apiURL).then((response) => {
+      setData(response.data[0]);
+    });
+  }
+
+  // const submitHandler = (e) => {
+  //   // Prevent form submission on Enter key
+  //   e.preventDefault();
+  // };
+
+  // const Synonym = ({ searchWord }) => <li>{searchWord}</li>;
+
+  return (
+    <div className="page">
+      <h1 className="page__header">Synonyms</h1>
+      <div className="page__search">
+        <label for="inp" className="inp">
+          <input
+            type="text"
+            placeholder="&nbsp;"
+            onChange={(event) => {
+              setSearchWord(event.target.value);
+            }}
+          />
+          <span className="label">Whats Another Word For...</span>
+          <span className="focus-bg"></span>
+        </label>
+
+        <button
+          className="page__button"
+          onClick={() => {
+            getSynonym();
+          }}
+        >
+          GO
+        </button>
+      </div>
+
+      {data && (
+        <div className="page__displayResults">
+          <h2 className="page__word">{data.word} </h2>
+          <h4 className="page__subtitle">Synonyms:</h4>
+          <h4 className="page__subtitle">Definition:</h4>
+          <p>{data.meanings[0].definitions[0].definition}</p>
+          <h4 className="page__subtitle">Parts of speech:</h4>
+          <p>{data.meanings[0].partOfSpeech}</p>
+          <h4 className="page__subtitle">Example:</h4>
+          <p>{data.meanings[0].definitions[0].example}</p>
+          {console.log(data.meanings[1].definitions[0].synonyms)}
+          {/* <p>{data.meanings[1].definitions[0].synonyms}</p> */}
+          {/* {data.meanings[1].definitions[0].synonyms}.map{} */}
+
+          {/* <ul>
+            {data.map((synonyms, i) => (
+              <li key={i}>{synonym}</li>
+            ))}
+          </ul> */}
+
+          {/* <ul>
+            {data.meanings[1].definitions[0].synonyms.map((searchWord, i) => (
+              <Synonym query={searchWord} key={i} />
+            ))}
+          </ul> */}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
